566e8f0af0c14de77eea753b5e9dfe3f3e06ec6c
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2025-04-17 20:16:45 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd /tmp
 echo $PASSWORD | sudo -S mkdir K-installer
+echo $PASSWORD | sudo -s chown 1000  K-installer
 cd K-installer
 git clone https://github.com/KAINAT-OS/K-SETTINGS.git
 cd K-SETTINGS/binary-settings/Builds/linux/
