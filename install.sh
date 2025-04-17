cd099b7dc87e0e067db15ae9dac6b77f62cd0c6a
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2025-04-17 18:13:30 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+cd /tmp
+mkdir /K-installer
+cd K-installer
+git clone https://github.com/KAINAT-OS/K-SETTINGS.git
+cd K-SETTINGS/binary-settings/Builds/linux/
+bash ./build.sh
+echo "$PASSWORD" | sudo -S dpkg -i ./*.deb
+cd /tmp/K-installer
+
+DEST_DIR="/usr/share/binary-updater/K-SETTINGS"
+SOURCE_FILE="version.sh"
+
+# Check if directory exists
+if [ ! -d "$DEST_DIR" ]; then
+    echo "Directory does not exist. Creating: $DEST_DIR"
+    sudo mkdir -p "$DEST_DIR"
+fi
+
+# Copy the file
+echo "Copying $SOURCE_FILE to $DEST_DIR"
+echo "$PASSWORD" | sudo -s  cp "$SOURCE_FILE" "$DEST_DIR"
+echo done
